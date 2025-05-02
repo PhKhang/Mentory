b636e8c32e7e0ca6150841f63fa8a1c4530f5f10
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2025-05-02 14:55:58 +0700

    Switch to jwt from scratch

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,37 +1,54 @@
 import { Request, Response } from "express";
-import { auth, authAdmin } from "../config/firebase";
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-  updateProfile,
-} from "firebase/auth";
+import bcrypt from "bcrypt";
+import jwt, { Secret, SignOptions } from "jsonwebtoken";
+import dotenv from "dotenv";
 import prisma from "../config/prisma";
 
+dotenv.config();
+
 const getUser = async (req: Request, res: Response) => {
-  const user = auth.currentUser;
-  authAdmin.updateUser
-  if (user) {
-    res.status(200).json({ message: "User is logged in", user });
-  } else {
-    res.status(401).json({ message: "User is not logged in" });
+  const token = req.cookies?.access_token;
+  
+  if (!token) {
+    console.log("No token")
+    res.status(401).json({ error: 'Not logged in' })
+    return 
   }
+
+  const decoded = jwt.verify(token, process.env.SECRET_KEY!)
+  console.log("Decoded token: ", decoded)
+  console.log("Username: ", (decoded as any)?.username)
+  res.json(decoded)
 };
 
 const verifyEmail = async (req: Request, res: Response) => {
   const { email } = req.body;
   try {
-    const user = await authAdmin.getUserByEmail(email);
-    if (user.emailVerified) {
-      res.status(200).json({ message: "Email is verified" });
-    } else {
-      res.status(200).json({ message: "Email is not verified" });
+    const userWithTheEmail = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (userWithTheEmail) {
+      res.status(400).json({ message: "Email is already used" });
+      return;
     }
+
+    res.status(200).json({ message: "Email is not used" });
   } catch (error: any) {
     const errorCode = error.code;
     const errorMessage = error.message;
     res.status(400).json({ errorCode, errorMessage });
   }
+};
+
+const createToken = (user: any, duration: any = "30d") => {
+  const payload = { id: "" };
+  const secret: Secret = process.env.JWT_SECRET as string;
+  const options: SignOptions = {
+    expiresIn: duration,
+  };
+
+  return jwt.sign(payload, secret, options);
 };
 
 const register = async (req: Request, res: Response) => {
@@ -53,19 +70,16 @@ const register = async (req: Request, res: Response) => {
   }
 
   try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
 
-    const user = userCredential;
     const usr = await prisma.user.create({
       data: {
-        firebaseId: user.user.uid,
-        email: user.user.email!,
+        firebaseId: "",
+        email: email,
         firstName: firstName,
         lastName: lastName,
+        passwordHash: hash,
       },
     });
 
@@ -76,7 +90,18 @@ const register = async (req: Request, res: Response) => {
       },
     });
 
-    res.status(200).json({ message: "User registered successfully", user });
+    const token = createToken(usr, "10m");
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      })
+      .status(200)
+      .json({ status: "logged in" });
+
+    // res.status(200).json({ message: "User registered successfully", usr });
   } catch (error: any) {
     const errorCode = error.code;
     const errorMessage = error.message;
@@ -88,14 +113,34 @@ const register = async (req: Request, res: Response) => {
 const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const user = userCredential.user;
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (!user) {
+      console.error("User not found:", email);
+      res.status(400).json({ message: "User not found" });
+      return;
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.passwordHash);
+    if (!isPasswordValid) {
+      console.error("Invalid password for user:", email);
+      res.status(400).json({ message: "Invalid password" });
+      return;
+    }
+
+    const token = createToken(user, "10m");
+
     console.log("User logged in successfully");
-    res.status(200).json({ message: "User logged in successfully", user });
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      })
+      .status(200)
+      .json({ status: "logged in" });
   } catch (error: any) {
     const errorCode = error.code;
     const errorMessage = error.message;
@@ -106,8 +151,7 @@ const login = async (req: Request, res: Response) => {
 
 const logout = async (req: Request, res: Response) => {
   try {
-    await signOut(auth);
-    res.status(200).json({ message: "User logged out successfully" });
+    res.clearCookie("access_token").status(200).redirect("/");
   } catch (error: any) {
     const errorCode = error.code;
     const errorMessage = error.message;
